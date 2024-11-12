feaa4bd5142aba8aeeb4107ed22c5dedf9cc69de
btwGaurav <gauravsingh.gs.gs123@gmail.com>
2024-11-12 14:11:11 +0530

    more practise

---
+++ one.js
@@ -0,0 +1,43 @@
+// for (i=1,i<=5;i++){
+//     console.log("1")
+// }
+// let str = "apnacollege";
+
+// for (let i of s6tr ){
+//     console.log("i=", i );
+// };
+
+// let student = {
+//     name :  "Rahul kumar" ,
+//     age : 20 ,
+//     cgpa : 7.5 ,
+//     ispass : true,
+// };
+
+// for (let key in student) {
+//     console.log( "key=" , key , "value=", student[key])  ;
+
+// }; 
+
+// for ( i=1;i<=100;i++){ if (i%2 === 0) {
+    
+
+//     console.log(i)
+// }};
+
+// let gameNum = 25;
+
+// let userNum = prompt ("Guess the game number :");
+
+// while (userNum != gameNum ) {//game 
+//     userNum =prompt ("You entred wrong no. guess agian :");
+//     }
+// console.log("congratulations you have entred the right number");
+
+     //string
+
+     let str ="Apnacollege";
+     console.log(str);
+     
+     console.log("apna\n college");
+     
